465fbe202109906bcfb5be4d5e4c01dbe7f4cc5c
cullback <cullback@fastmail.com>
2025-05-11 11:34:06 -0400

    add grammar and print tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use pest::Parser;
+use pest_derive::Parser;
+use std::env;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct MyParser;
+
 fn main() {
     println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    let content = std::fs::read_to_string(&args[1]).expect("file does not exist");
+
+    let tree = MyParser::parse(Rule::program, &content);
+
+    println!("{tree:?}");
 }
