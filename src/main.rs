dc9eb42889deb7bbc67a6f28748b130880c9742e
cullback <cullback@fastmail.com>
2025-05-12 17:31:47 -0400

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use abstract_syntax_tree::parse_program;
 use pest::Parser;
 use pest_derive::Parser;
 use std::env;
-use std::process;
 
 mod abstract_syntax_tree;
 
@@ -13,7 +12,7 @@ struct MyParser;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let content = std::fs::read_to_string(&args[1]).unwrap();
-    let mut pairs = MyParser::parse(Rule::program, &content).unwrap();
+    let pairs = MyParser::parse(Rule::program, &content).unwrap();
 
     let ast = parse_program(pairs);
 
