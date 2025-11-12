a5b5112a61ce2fe55ae89f01941a3a3e99709d67
cullback <cullback@fastmail.com>
2025-11-12 05:26:53 +0000

    Add 164 byte hello-world in aarch64

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-use abstract_syntax_tree::parse_program;
-use pest::Parser;
-use pest_derive::Parser;
-use std::env;
-
-mod abstract_syntax_tree;
-
-#[derive(Parser)]
-#[grammar = "grammar.pest"]
-struct MyParser;
+mod smol_hello;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let content = std::fs::read_to_string(&args[1]).unwrap();
-    let pairs = MyParser::parse(Rule::program, &content).unwrap();
-
-    let ast = parse_program(pairs);
-
-    println!("{ast:#?}");
+    smol_hello::write_aarch64_hello().unwrap();
 }
